4988217b1478c3d6612d35146c382fd9d52cda5c
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-19 11:40:37 +0530

    Feed componenets

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,22 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
         Discover & Share
-        <br className="max-md:hidden "/>
+        <br className="max-md:hidden " />
         <span className="orange_gradient">AI-Powered Prompt</span>
       </h1>
       <p className="desc text-center">
-        ZenPrompt is an open-source Ai prompting tool for modern world to discover, create and share creative prompts.
+        ZenPrompt is an open-source Ai prompting tool for modern world to
+        discover, create and share creative prompts.
       </p>
+
+      {/* Feed */}
+      <Feed />
     </section>
-  )
+  );
 };
 
 export default Home;
