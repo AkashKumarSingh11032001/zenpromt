80fc069600f7d315225ced5603a04c09fcba1be9
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-19 11:58:34 +0530

    Nav bar designing

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProvider } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -17,7 +18,34 @@ const Nav = () => {
           height={30}
           className="object-containe"
         />
+        <p className="logo_text">Zen Prompt</p>
       </Link>
+
+      {/* Desktop navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button onClick={signOut} className="outline_btn">
+              Logout
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                alt="profile icon"
+                width={37}
+                height={37}
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+          </>
+        )}
+      </div>
     </nav>
   );
 };
